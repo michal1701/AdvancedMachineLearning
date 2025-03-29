56ad69dd401b584b3e993a5f0d9409c2c6fec344
Michal <legczylin.michal@gmail.com>
2025-03-29 02:42:11 +0100

    LogRegCCD implemented

---
+++ Task2/implementation/measures.py
@@ -0,0 +1,128 @@
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+
+from sklearn.metrics import recall_score, precision_score, f1_score, balanced_accuracy_score, roc_auc_score, precision_recall_curve
+
+class IMeasure(ABC):
+    @staticmethod
+    @abstractmethod
+    def get(true, pred):
+        return
+
+    @staticmethod
+    @abstractmethod
+    def get_name():
+        return
+
+    @staticmethod
+    @abstractmethod
+    def get_type():
+        return
+
+class Measure:
+    class Type(Enum):
+        recall = auto()
+        precision = auto()
+        F1_score = auto()
+        balanced_accuracy = auto()
+        AUC_ROC = auto()
+        AUC_PR = auto()
+
+    @staticmethod
+    def from_type(p_type: "Measure.Type"):
+        assert isinstance(p_type, Measure.Type)
+        
+        match p_type:
+            case Measure.Type.recall:
+                return Measure.Recall()
+            case Measure.Type.precision:
+                return Measure.Precision()
+            case Measure.Type.F1_score:
+                return Measure.F1Score()
+            case Measure.Type.balanced_accuracy:
+                return Measure.BalancedAccuracy()
+            case Measure.Type.AUC_ROC:
+                return Measure.AUC_ROC()
+            case Measure.Type.AUC_PR:
+                return Measure.AUC_PR()
+            case _:
+                raise Exception(f"Unhandled type '{p_type}' passed")
+
+    class Recall(IMeasure):
+        @staticmethod
+        def get(true, pred):
+            return recall_score(true, pred)
+
+        @staticmethod
+        def get_name():
+            return "recall"
+    
+        @staticmethod
+        def get_type():
+            return Measure.Type.recall
+    
+    class Precision(IMeasure):
+        @staticmethod
+        def get(true, pred):
+            return precision_score(true, pred)
+
+        @staticmethod
+        def get_name():
+            return "precision"
+    
+        @staticmethod
+        def get_type():
+            return Measure.Type.precision
+    
+    class F1Score(IMeasure):
+        @staticmethod
+        def get(true, pred):
+            return f1_score(true, pred)
+
+        @staticmethod
+        def get_name():
+            return "F1-score"
+    
+        @staticmethod
+        def get_type():
+            return Measure.Type.F1_score
+    
+    class BalancedAccuracy(IMeasure):
+        @staticmethod
+        def get(true, pred):
+            return balanced_accuracy_score(true, pred)
+
+        @staticmethod
+        def get_name():
+            return "balanced accuracy"
+    
+        @staticmethod
+        def get_type():
+            return Measure.Type.balanced_accuracy
+    
+    class AUC_ROC(IMeasure):
+        @staticmethod
+        def get(true, pred):
+            return roc_auc_score(true, pred)
+
+        @staticmethod
+        def get_name():
+            return "auc roc"
+    
+        @staticmethod
+        def get_type():
+            return Measure.Type.AUC_ROC
+    
+    class AUC_PR(IMeasure):
+        @staticmethod
+        def get(true, pred):
+            """ it returns (precision, recall, thresholds), not one value """
+            return precision_recall_curve(true, pred)
+
+        @staticmethod
+        def get_name():
+            return "auc pr"
+    
+        @staticmethod
+        def get_type():
+            return Measure.Type.AUC_PR
